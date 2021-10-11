467a97f26d7feb8cb41c77851c4891b7cdd4cd5a
Sevde Oguz <sevdeoguz@Sevdes-MacBook-Pro.local>
2021-10-11 14:51:26 +0200

    responsive

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,31 +9,52 @@
     <body>
         <header>
         </header>
-        <main>
+          <main>
           <p>
           <va-button class="mr-2 mb-2" @click="showModalSizeLarge = !showModalSizeLarge">
             Show modal size large
           </va-button>
-          <va-modal v-model="showModalSizeLarge" :message="message" size="large">
-            <div style="height: 13rem; border: 2px solid var(--va-primary); width:900px;">
+          <va-modal v-model="showModalSizeLarge" :message="message">
+            <div style="height: 13rem; border: 2px solid var(--va-primary); width:600px;">
             <va-sidebar textColor="dark">
               <va-sidebar-item>
                 <va-sidebar-item-content>
-                  <va-icon name="dashboard" @click="showSlide1"/>
+                  <va-icon name="dashboard"/>
                   <!-- User can hide item with css if he wants -->
                   <va-sidebar-item-title> Velg behandling </va-sidebar-item-title>
                 </va-sidebar-item-content>
               </va-sidebar-item>
-              <va-sidebar-item active>
+              <va-sidebar-item>
                 <va-sidebar-item-content>
                   <va-icon name="room" />
                   <va-sidebar-item-title> Velg klient </va-sidebar-item-title>
                 </va-sidebar-item-content>
               </va-sidebar-item>
+               <va-sidebar-item>
+                <va-sidebar-item-content>
+                <va-icon name="dashboard"/>
+                  <!-- User can hide item with css if he wants -->
+                  <va-sidebar-item-title> Velg Dato og kl </va-sidebar-item-title>
+                </va-sidebar-item-content>
+              </va-sidebar-item>
+              <va-sidebar-item>
+                <va-sidebar-item-content>
+                <va-icon name="dashboard"/>
+                  <!-- User can hide item with css if he wants -->
+                  <va-sidebar-item-title> Skriv informasjon! </va-sidebar-item-title>
+                </va-sidebar-item-content>
+              </va-sidebar-item>
+              <va-sidebar-item>
+                <va-sidebar-item-content>
+                <va-icon name="dashboard"/>
+                  <!-- User can hide item with css if he wants -->
+                  <va-sidebar-item-title> Bekreft </va-sidebar-item-title>
+                </va-sidebar-item-content>
+              </va-sidebar-item>
             </va-sidebar>
           </div>
           </va-modal>
-        </p>
+          </p>
         </main>
         <footer>
         </footer>
