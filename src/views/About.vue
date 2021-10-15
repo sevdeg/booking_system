394f1326a5a986baa85d61ec966319307c5e8cb5
Suhail0310000 <s341868@oslomet.no>
2021-10-15 11:51:52 +0200

    to res div boxes

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,35 +10,21 @@
         <header>
         </header>
           <main>
-          <p>
           <va-button class="mr-2 mb-2" @click="showModalSizeLarge = !showModalSizeLarge">
             Show modal size large
           </va-button>
           <va-modal v-model="showModalSizeLarge" :message="message">
-            <!-- <div class="row">
-                <div class="col-xs-12 col-md-3">.col-xs-12 .col-md-3
-
+            <va-card color="background" style="padding: 0.75rem;">
+              <div class="row">
+                <div class="flex md6">
+                  <div class="item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                 </div>
-                <div class="col-xs-12 col-md-3">.col-xs-12 .col-md-3
-
+                <div class="flex md6">
+                  <div class="item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                 </div>
-                <div class="col-xs-12 col-md-3">.col-xs-12 .col-md-3
-
-                </div>
-                <div class="col-xs-12 col-md-3">.col-xs-12 .col-md-3
-
-                </div>
-            </div> -->
-             <div class="row">
-                <div class="flex md6 lg4">
-                <va-card color="primary" gradient>
-                    <va-card-title>Title</va-card-title>
-                    <va-card-content>Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit.</va-card-content>
-                </va-card>
-                </div>
-            </div>
+              </div>
+            </va-card>
           </va-modal>
-          </p>
         </main>
         <footer>
         </footer>
@@ -50,8 +36,16 @@ export default {
   data () {
     return {
       showModalSizeLarge: false
+      // value1: 45
     }
   }
 }
 
 </script>
+<style scoped>
+  .item {
+    border: 1px solid rgb(212, 205, 205);
+    background-color: #ffffff;
+    text-align: center;
+  }
+</style>
